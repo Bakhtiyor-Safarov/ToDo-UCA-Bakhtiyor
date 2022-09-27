8cf74c46839b1f3a656d781839d85a929f93dc75
Бахтиёр Сафаров <BakhtiyorS@corp.faiza.tj>
2022-09-27 18:16:02 +0500

    upload app

---
+++ Todo/todoapp/todolist/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Todo(models.Model):
+
+    title = models.CharField('Название задания', max_length=500)
+    is_complete = models.BooleanField('Завершено', default=False)
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Звадания'
+
+    def __str__(self):
+        return self.title
+
+
